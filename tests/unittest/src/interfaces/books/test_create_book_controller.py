90ff2c18bf2a611ee3db8b366429d9cccd253910
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-18 22:57:19 -0300

    Test create_books_controller

--- tests/unittest/src/interfaces/books/test_create_book_controller.py
+++ tests/unittest/src/interfaces/books/test_create_book_controller.py
@@ -1,8 +1,10 @@
 from types import NoneType
 import pytest
 from src.application.interfaces.book_request_interface import BookRequestInterface
+from src.application.interfaces.books_respository_interface import BooksRepositoryInterface
 
 from src.interfaces.books.create_book_controller import CreateBookController
+from src.application.usecases.books.create_books import CreateBooks
 
 
 @pytest.fixture
@@ -11,9 +13,24 @@ def book_request():
         pass
     return RequestBooks()
 
+
 @pytest.fixture
-def create_book_controller():
-    return CreateBookController(controller=None, repository=None)
+def test_create_book_controller():  
+    return CreateBookController
+
+@pytest.fixture
+def book_repository():
+    class BookRepository(BooksRepositoryInterface):
+        pass 
+    return BookRepository
+
+@pytest.fixture
+def usecase_create_book():
+    return CreateBooks
+
+@pytest.fixture
+def create_book_controller(usecase_create_book, book_repository):
+    return CreateBookController(controller=usecase_create_book, repository=book_repository)
 
 
 def test_should_be_has_methods(create_book_controller) -> None:
